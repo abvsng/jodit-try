c5a311bf3b8b4022182327f0621a1acaf2fe8a29
anvDT <devabvsng@gmail.com>
2025-02-15 11:12:47 +0530

    submit setup

--- jodit/src/App.jsx
+++ jodit/src/App.jsx
@@ -5,6 +5,7 @@ import JoditEditor from "jodit-react";
 function App() {
   const [theme, setTheme] = useState(false);
   const [content, setContent] = useState("");
+  const [blog, setBlog] = useState("");
   const editorRef = useRef(null);
   const config = useMemo(
     () => ({
@@ -21,6 +22,7 @@ function App() {
         onClick={() => {
           setTheme((p) => !p);
         }}
+        title="Editor theme"
       >
         {theme ? "Dark" : "Light"}
       </button>
@@ -31,6 +33,24 @@ function App() {
         onChange={(newContent) => {
           setContent(newContent);
         }}
+      />
+      <button
+        onClick={() => {
+          setBlog(content);
+        }}
+        title="Submit content"
+      >
+        Submit
+      </button>
+
+      <div
+        style={{
+          margin: "auto",
+          border: "solid black",
+          padding: 5,
+          width: 800,
+        }}
+        dangerouslySetInnerHTML={{ __html: blog }}
       />
     </>
   );
